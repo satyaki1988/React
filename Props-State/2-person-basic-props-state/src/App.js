a73f75963736f73fa02b7452bd5b2733e31f8d53
Satyaki Mukherjee <satyaki.mukherji@in.ibm.com>
2019-05-24 20:30:37 +0530

    state in react

---
+++ Props-State/2-person-basic-props-state/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './components/Person/Person';
+
+class App extends Component {
+  state = {
+    person: [
+      { name: 'Satyaki', designation: 'App Developer', organization: 'IBM' },
+      { name: 'Sumon', designation: 'Project Manager', organization: 'Air BP' },
+      { name: 'Ayan', designation: 'Scrum Master', organization: 'Ad Hoc 24' },
+      { name: 'Natasha', designation: 'Lead Developer', organization: 'IBM India' },
+      { name: 'Sinijini', designation: 'Tester', organization: 'Digital BP' }
+    ],
+    otherState: 'He taught us so much :('
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Welcome to the React development!!!</h1>
+        </div>
+        <Person
+          name={this.state.person[0].name}
+          designation={this.state.person[0].designation}
+          organization={this.state.person[0].organization} />
+        <Person
+          name={this.state.person[1].name}
+          designation={this.state.person[1].designation}
+          organization={this.state.person[1].organization} >
+          {this.state.otherState}
+        </Person>
+        <Person
+          name={this.state.person[2].name}
+          designation={this.state.person[2].designation}
+          organization={this.state.person[2].organization} />
+        <Person
+          name={this.state.person[3].name}
+          designation={this.state.person[3].designation}
+          organization={this.state.person[3].organization} />
+      </div>
+    );
+  }
+}
+
+export default App;
